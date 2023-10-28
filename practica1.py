d0fc7301462f9eb51b9d7ecc0ac4816af5ce1731
EryonVF <146021991+EryonVF@users.noreply.github.com>
2023-10-27 17:48:00 -0800

    Add files via upload

--- practica1.py
+++ practica1.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/welcome/<name>/<ncontrol>')
-def hello(name,ncontrol):
-    return f'Bienvenido ' + name + ncontrol
+@app.route('/wellcome/')
+@app.route('/wellcome/<name>')
+@app.route('/wellcome/<int:ncontrol>')
+@app.route('/wellcome/<name>/<int:ncontrol>')
+def bienvenido(name=None,ncontrol=None):
+    if name== None and ncontrol==None:
+        return 'Bienvenido '
+    if name!= None and ncontrol == None:
+        return f'Bienvenido {name}'
